122d12e3cb0321b7833cfbc2ab23fe77367c6f04
Alex Andrews <andrewsalex577@gmail.com>
2023-01-22 15:00:50 +0000

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Alex Andrews on 22/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
