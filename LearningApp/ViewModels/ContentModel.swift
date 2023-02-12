3be550a290bafec7816cf34d770acb174bce4f67
Alex Andrews <andrewsalex577@gmail.com>
2023-02-12 13:09:54 +0000

    - submit button in TestView - highlight appropriate colours

--- LearningApp/ViewModels/ContentModel.swift
+++ LearningApp/ViewModels/ContentModel.swift
@@ -9,10 +9,10 @@ import Foundation
 
 class ContentModel: ObservableObject {
     
-    // list of modules
+    // List of modules
     @Published var modules = [Module]()
     
-    // Current Module
+    // Current module
     @Published var currentModule: Module?
     var currentModuleIndex = 0
     
@@ -20,102 +20,115 @@ class ContentModel: ObservableObject {
     @Published var currentLesson: Lesson?
     var currentLessonIndex = 0
     
-    // current Question
+    // Current question
     @Published var currentQuestion: Question?
     var currentQuestionIndex = 0
     
     // Current lesson explanation
-    @Published var codeText  = NSAttributedString()
+    @Published var codeText = NSAttributedString()
     var styleData: Data?
     
-    // current selected content and test
+    // Current selected content and test
     @Published var currentContentSelected:Int?
     @Published var currentTestSelected:Int?
     
+    
     init() {
+        
         getLocalData()
+        
     }
     
-    // Mark: - Data methods
+    // MARK: - Data methods
     
     func getLocalData() {
         
-        // Get url to json file
+        // Get a url to the json file
         let jsonUrl = Bundle.main.url(forResource: "data", withExtension: "json")
         
         do {
-            // read file into data object
+            // Read the file into a data object
             let jsonData = try Data(contentsOf: jsonUrl!)
             
-            // try to decode json into an array of modules
+            // Try to decode the json into an array of modules
             let jsonDecoder = JSONDecoder()
             let modules = try jsonDecoder.decode([Module].self, from: jsonData)
             
-            // assign parsed modules to muldes property
+            // Assign parsed modules to modules property
             self.modules = modules
         }
         catch {
-            print("couldnt parse local data")
+            // TODO log error
+            print("Couldn't parse local data")
         }
         
+        // Parse the style data
         let styleUrl = Bundle.main.url(forResource: "style", withExtension: "html")
-
+        
         do {
             
-            // read the file into a data object
+            // Read the file into a data object
             let styleData = try Data(contentsOf: styleUrl!)
             
             self.styleData = styleData
         }
         catch {
-            print("couldnt parse style data")
+            // Log error
+            print("Couldn't parse style data")
         }
         
     }
     
-    // Mark: - Module navigation methods
+    // MARK: - Module navigation methods
     
     func beginModule(_ moduleid:Int) {
-        // find the index for this module id
+        
+        // Find the index for this module id
         for index in 0..<modules.count {
+            
             if modules[index].id == moduleid {
-                
-                // find the mathcing module
+            
+                // Found the matching module
                 currentModuleIndex = index
                 break
             }
         }
         
-        // set the current module
+        // Set the current module
         currentModule = modules[currentModuleIndex]
     }
     
     func beginLesson(_ lessonIndex:Int) {
-        // check that the lesson is in range of module lessons
+        
+        // Check that the lesson index is within range of module lessons
         if lessonIndex < currentModule!.content.lessons.count {
             currentLessonIndex = lessonIndex
-        } else {
+        }
+        else {
             currentLessonIndex = 0
         }
         
+        // Set the current lesson
         currentLesson = currentModule!.content.lessons[currentLessonIndex]
         codeText = addStyling(currentLesson!.explanation)
     }
     
     func nextLesson() {
-        // advance the lesson
+        
+        // Advance the lesson index
         currentLessonIndex += 1
         
-        // check whether it is in range
+        // Check that it is within range
         if currentLessonIndex < currentModule!.content.lessons.count {
-            // set the current lesson property
+            
+            // Set the current lesson property
             currentLesson = currentModule!.content.lessons[currentLessonIndex]
             codeText = addStyling(currentLesson!.explanation)
         }
         else {
-            // reset the lesson state
-            currentLesson = nil
+            // Reset the lesson state
             currentLessonIndex = 0
+            currentLesson = nil
         }
     }
     
@@ -125,41 +138,42 @@ class ContentModel: ObservableObject {
     }
     
     func beginTest(_ moduleId:Int) {
-        // set the current module
+        
+        // Set the current module
         beginModule(moduleId)
         
-        // set the current question
+        // Set the current question index
         currentQuestionIndex = 0
         
-        // if there are questions, set the current question to the first one
-        if currentModule?.test.questions.count ?? 0 > 0 {
+        // If there are questions, set the current question to the first one
+        if currentModule?.test.questions.count ?? 0  > 0 {
             currentQuestion = currentModule!.test.questions[currentQuestionIndex]
-            // set the question content
+            
+            // Set the question content
             codeText = addStyling(currentQuestion!.content)
         }
     }
     
-    // Mark: Code Styling
+    // MARK: - Code Styling
+    
     private func addStyling(_ htmlString: String) -> NSAttributedString {
         
         var resultString = NSAttributedString()
         var data = Data()
         
-        // add styling data
+        // Add the styling data
         if styleData != nil {
-            data.append(self.styleData!)
+            data.append(styleData!)
         }
         
-        // add html data
+        // Add the html data
         data.append(Data(htmlString.utf8))
         
-        
-        
-        // convert to attributed string
-            if let attributedString = try? NSAttributedString(data: data, options: [.documentType: NSAttributedString.DocumentType.html], documentAttributes: nil) {
-                
-                resultString = attributedString
-            }
+        // Convert to attributed string
+        if let attributedString = try? NSAttributedString(data: data, options: [.documentType: NSAttributedString.DocumentType.html], documentAttributes: nil) {
+            
+            resultString = attributedString
+        }
         
         return resultString
     }
--- LearningApp/Views/TestView.swift
+++ LearningApp/Views/TestView.swift
@@ -10,22 +10,105 @@ import SwiftUI
 struct TestView: View {
     
     @EnvironmentObject var model:ContentModel
+    @State var selectedAnswerIndex:Int?
+    @State var numCorrect = 0
+    @State var submitted = false
     
     var body: some View {
+        
         if model.currentQuestion != nil {
+            
             VStack {
                 // Question number
                 Text("Question \(model.currentQuestionIndex + 1) of \(model.currentModule?.test.questions.count ?? 0)")
+                    .padding(.leading, 20)
                 
-                // question
+                // Question
                 CodeTextView()
+                    .padding(.horizontal, 20)
                 
-                // answers
+                // Answers
+                ScrollView {
+                    VStack {
+                        ForEach (0..<model.currentQuestion!.answers.count, id: \.self) { index in
+                            
+                            Button {
+                                // Track the selected index
+                                selectedAnswerIndex = index
+                                
+                            } label: {
+                                ZStack {
+                                    
+                                    if submitted == false {
+                                        RectangleCard(color: index == selectedAnswerIndex ? .gray : .white)
+                                            .frame(height: 48)
+                                    }
+                                    else {
+                                        // answer has been submitted
+                                        if index == selectedAnswerIndex && index == model.currentQuestion!.correctIndex {
+                                            
+                                            // user has selected the right answer
+                                            // show green background
+                                            RectangleCard(color: Color.green)
+                                                .frame(height: 48)
+                                        }
+                                        else if index == selectedAnswerIndex && index != model.currentQuestion!.correctIndex {
+                                            
+                                            // user selected wrong answer
+                                            // show red background
+                                            RectangleCard(color: Color.red)
+                                                .frame(height: 48)
+                                        }
+                                        else if index == model.currentQuestion!.correctIndex {
+                                                // this button is the corretct answer
+                                                // show a green background
+                                                RectangleCard(color: Color.red)
+                                                    .frame(height: 48)
+                                            
+                                        }
+                                        else {
+                                            RectangleCard(color: Color.white)
+                                                .frame(height: 48)
+                                        }
+                                    }
+                                    
+                                    Text(model.currentQuestion!.answers[index])
+                                }
+                            }
+                            .disabled(submitted)
+                        }
+                    }
+                    .accentColor(.black)
+                    .padding()
+                }
                 
-                // button
+                // submit Button
+                Button {
+                    // change submitted state to true
+                    submitted = true
+                    
+                    // check if correct and
+                    if selectedAnswerIndex == model.currentQuestion!.correctIndex {
+                        numCorrect += 1
+                    }
+                    
+                } label: {
+                    ZStack {
+                        RectangleCard(color: .green)
+                            .frame(height: 48)
+                        
+                        Text("Submit")
+                            .bold()
+                            .foregroundColor(Color.white)
+                    }
+                    .padding()
+                }
+                .disabled(selectedAnswerIndex == nil)
             }
             .navigationBarTitle("\(model.currentModule?.category ?? "") Test")
+            
         }
+        
     }
 }
 
